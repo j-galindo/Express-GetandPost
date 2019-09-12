6d8b51292698e63ef26a64769a541a8d54352cbe
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-11 21:05:42 -0400

    follow along

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res) {
+    console.log(req);
+})
+
+app.listen(3000, () => console.log('App listening on port 3000!'));
